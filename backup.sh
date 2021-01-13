db2a43ec158f1cbe3fcf7ee0f0afcabc60101054
Shad Beard <shadbeard81@gmail.com>
2021-01-12 22:40:28 -0500

    Testing something else

--- backup.sh
+++ backup.sh
@@ -18,6 +18,7 @@ if [ $? -eq 0 ]; then
 	echo "_OUTPUT_ dir"
 	ls -l /opt/grafana-backup-tool/_OUTPUT_/
 
+	gsutil ls
 	# gsutil cp /opt/grafana-backup-tool/_OUTPUT_/* gs://$GRAFANA_BACKUP_BUCKET/
 else
 	echo 'FAILED'
